5fbd9fb3e5c7d63da08905935790654119c155ab
Jeremiah Ekundayo <dayoolapeju@outlook.com>
2022-12-03 08:23:56 +0100

    Add req.body middleware

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = process.env.port || 8000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.get("/", (req, res) => res.send("This is CutSession API"));
 
 app.listen(port, () => {
