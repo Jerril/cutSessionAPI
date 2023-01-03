bfa6708ead984e3004df631fa651b00221dc9f32
Jeremiah Ekundayo <dayoolapeju@outlook.com>
2023-01-03 23:54:35 +0100

    Little tweaks

--- utils/constants.js
+++ utils/constants.js
@@ -2,21 +2,16 @@
  * @desc This file defines the app constants
  */
 
-class AccessType {
+exports.AccessType = class {
     static user = 'user';
     static merchant = 'merchant';
 
     static all = [this.user, this.merchant];
 }
 
-class SessionType {
+exports.SessionType = class {
     static weekday = 'weekDay';
     static weekend = 'weekEnd';
 
     static all = [this.weekday, this.weekend];
-}
-
-module.exports = {
-    AccessType,
-    SessionType
 }
